3eade9a0d0be8ea2d5ce2cdbdba9ab1a660bca80
theteachr <sop.theteachr@gmail.com>
2021-11-09 02:11:21 +0530

    Create players

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,11 @@ mod game;
 fn main() {
     let mut main_game = game::Game::new(4);
 
+    let _red = player::Player::new(String::from("1415"), String::from("Red"));
+    let _henry = player::Player::new(String::from("1416"), String::from("Henry"));
+    let _snotty = player::Player::new(String::from("1417"), String::from("Snotty"));
+    let _matilda = player::Player::new(String::from("1418"), String::from("Matilda"));
+
     println!("Shuffled {} cards", main_game.draw_pile.len());
 
     let drawn_cards = main_game.draw_pile.draw(deck::DrawCount::Two);
