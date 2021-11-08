e59bb8be5c4e5381fd00061f594ee639b2ba1ec4
theteachr <sop.theteachr@gmail.com>
2021-11-09 00:05:45 +0530

    Pretty print

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,6 @@ fn main() {
     let theteachr = player::Player::new(String::from("8100"), String::from("theteachr"));
     let game = game::Game::new(4);
 
-    println!("{:?}", theteachr);
-    println!("{:?}", game);
+    println!("{:#?}", theteachr);
+    println!("{:#?}", game);
 }
