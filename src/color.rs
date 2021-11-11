dc5969277d6b67118b2f133ebc6423dd965b2e03
theteachr <sop.theteachr@gmail.com>
2021-11-11 22:47:18 +0530

    Implement `to_string` on Color

--- src/color.rs
+++ src/color.rs
@@ -14,29 +14,31 @@ pub enum Color {
 	Yellow,
 }
 
-fn to_str(variant: Color) -> &'static str {
-	match variant {
-		Color::Black => "Black",
-		Color::Blue => "Blue",
-		Color::Brown => "Brown",
-		Color::Green => "Green",
-		Color::LightBlue => "Light Blue",
-		Color::LightGreen => "Light Green",
-		Color::Orange => "Orange",
-		Color::Pink => "Pink",
-		Color::Red => "Red",
-		Color::Yellow => "Yellow",
+impl Color {
+	fn to_string(&self) -> &'static str {
+		match self {
+			Color::Black => "Black",
+			Color::Blue => "Blue",
+			Color::Brown => "Brown",
+			Color::Green => "Green",
+			Color::LightBlue => "Light Blue",
+			Color::LightGreen => "Light Green",
+			Color::Orange => "Orange",
+			Color::Pink => "Pink",
+			Color::Red => "Red",
+			Color::Yellow => "Yellow",
+		}
 	}
 }
 
 impl fmt::Debug for Color {
 	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-		write!(f, "Color {}", to_str(*self))
+		write!(f, "Color {}", self.to_string())
 	}
 }
 
 impl fmt::Display for Color {
 	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-		write!(f, "{}", to_str(*self))
+		write!(f, "{}", self.to_string())
 	}
 }
