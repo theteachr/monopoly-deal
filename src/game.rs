23a6b2cb2c3eca65ff902de3530aa0d97713586c
theteachr <sop.theteachr@gmail.com>
2021-11-11 21:51:42 +0530

    Mock players

--- src/game.rs
+++ src/game.rs
@@ -14,18 +14,25 @@ pub struct Game<'a> {
 
 impl Game<'_> {
 	pub fn new(num_players: u8) -> Self {
-		let mut players = Vec::new();
 		let mut draw_pile = Deck::new();
 
-		// read players
-		for id in 0..num_players {
-			players.push(Player::read(id as usize));
-		}
+		println!("Shuffled {} cards.", draw_pile.len());
+
+		let mut players = get_mock_players();
+
+		println!("Added {} players. {:#?}", players.len(), players);
 
 		// distribute cards
 		for player in &mut players {
 			player.update_hand(draw_pile.draw(DrawCount::Five));
 		}
+
+		println!(
+			"Gave 5 cards each. {} cards left in the deck.",
+			draw_pile.len()
+		);
+
+		println!("{:#?}", players);
 
 		Game {
 			draw_pile,
@@ -33,4 +40,14 @@ impl Game<'_> {
 			players,
 		}
 	}
+}
+
+fn get_mock_players() -> Vec<Player> {
+	let names = ["Red", "Matilda", "Bomb", "Henry"];
+
+	names
+		.iter()
+		.enumerate()
+		.map(|(i, name)| Player::new(i, String::from(*name), HashSet::new()))
+		.collect()
 }
