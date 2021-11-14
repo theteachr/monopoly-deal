b6901d7032fe24a0847625bbafbe96268db84582
theteachr <sop.theteachr@gmail.com>
2021-11-15 00:19:24 +0530

    Use colors

--- src/cards/property_card.rs
+++ src/cards/property_card.rs
@@ -1,23 +1,25 @@
 use std::fmt;
 
 use crate::cards::rent_vec::RentVec;
-use crate::color;
+use crate::color::Color;
 
 use std::{
 	cmp::PartialEq,
 	hash::{Hash, Hasher},
 };
 
+use crossterm::style::Stylize;
+
 #[derive(Debug)]
 pub struct PropertyCard {
 	value: u8,
 	pub title: &'static str,
-	pub color: color::Color,
+	pub color: Color,
 	pub rents: RentVec,
 }
 
 impl PropertyCard {
-	pub fn new(value: u8, title: &'static str, color: color::Color, rents: RentVec) -> Self {
+	pub fn new(value: u8, title: &'static str, color: Color, rents: RentVec) -> Self {
 		Self {
 			value,
 			title,
@@ -43,6 +45,19 @@ impl Eq for PropertyCard {}
 
 impl fmt::Display for PropertyCard {
 	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-		write!(f, "'{}'", self.title)
+		let text = match self.color {
+			Color::Black	   => self.title.dark_grey(),
+			Color::Blue		   => self.title.dark_blue(),
+			Color::Brown	   => self.title.grey(),
+			Color::Green	   => self.title.dark_green(),
+			Color::LightBlue   => self.title.blue(),
+			Color::LightGreen  => self.title.green(),
+			Color::Orange	   => self.title.dark_yellow(),
+			Color::Pink		   => self.title.magenta(),
+			Color::Red		   => self.title.red(),
+			Color::Yellow	   => self.title.yellow(),
+		};
+
+		write!(f, "{}", text)
 	}
 }
