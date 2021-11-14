4b50c4b31468bce8fbcb60806fca57bd0a073128
theteachr <sop.theteachr@gmail.com>
2021-11-15 00:25:48 +0530

    Align

--- src/cards/money_card.rs
+++ src/cards/money_card.rs
@@ -3,12 +3,12 @@ use std::fmt;
 #[derive(Clone, Copy, Debug, Hash, PartialEq, Eq)]
 #[repr(u8)]
 enum Denomination {
-	One = 1,
-	Two = 2,
-	Three = 3,
-	Four = 4,
-	Five = 5,
-	Ten = 10,
+	One    = 1,
+	Two    = 2,
+	Three  = 3,
+	Four   = 4,
+	Five   = 5,
+	Ten    = 10,
 }
 
 #[derive(Debug, Hash, Eq, PartialEq)]
@@ -23,13 +23,13 @@ pub struct MoneyCard {
 impl From<u8> for Denomination {
 	fn from(value: u8) -> Self {
 		match value {
-			1 => Self::One,
-			2 => Self::Two,
-			3 => Self::Three,
-			4 => Self::Four,
-			5 => Self::Five,
+			 1 => Self::One,
+			 2 => Self::Two,
+			 3 => Self::Three,
+			 4 => Self::Four,
+			 5 => Self::Five,
 			10 => Self::Ten,
-			_ => unreachable!("Invalid Denomination"),
+			 _ => unreachable!("Invalid Denomination"),
 		}
 	}
 }
