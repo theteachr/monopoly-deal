2ee90a54a42df95398996098262dd009e6f0a4e8
theteachr <sop.theteachr@gmail.com>
2021-12-29 23:46:53 +0530

    Extact `input` into the `common` crate

---
+++ src/common.rs
@@ -0,0 +1,14 @@
+use std::io::{stdin, stdout, Write};
+
+pub fn input(prompt: &str) -> String {
+	let mut input = String::new();
+
+	print!("{}", prompt);
+	stdout().flush().expect("Couldn't flush :<");
+
+	stdin()
+		.read_line(&mut input)
+		.expect("Couldn't read from `stdin` :<");
+
+	return input;
+}
