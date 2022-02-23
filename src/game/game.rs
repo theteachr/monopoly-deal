3b472ae7b25e42c462061c1ebdd76438398d1f84
theteachr <sop.theteachr@gmail.com>
2022-02-24 02:55:52 +0530

    Move `Turn` into its own mod

--- src/game/game.rs
+++ src/game/game.rs
@@ -1,84 +1,13 @@
 use crate::{cards::Colored, color::CardColor, common::input, deck::Deck, player::Player};
+use super::Turn;
 
 use std::collections::VecDeque;
 use std::fmt::Debug;
 
-struct Turn {
-	player: Player,
-	num_cards_played: u8,
-}
-
-enum PlayerAction {
+pub(crate) enum PlayerAction {
 	Play(u8),
 	Pass,
 	// TODO Rearrange
-}
-
-impl Turn {
-	pub fn new(player: Player) -> Self {
-		Self {
-			player,
-			num_cards_played: 0,
-		}
-	}
-
-	pub fn read_action(&mut self) -> PlayerAction {
-		if self.num_cards_played == 3 {
-			return PlayerAction::Pass;
-		}
-
-		self.player.print_assets();
-		self.player.hand.print_numbered();
-
-		let user_input = input("> ");
-		let trimmed = user_input.trim();
-
-		if trimmed.is_empty() {
-			return PlayerAction::Pass;
-		}
-
-		loop {
-			match trimmed.parse::<u8>() {
-				Ok(n) => break PlayerAction::Play(n),
-				Err(_) => continue,
-			}
-		}
-	}
-
-	fn play(&mut self, card_position: u8, table: &mut VecDeque<Player>) {
-		if let Some(n) = self
-			.player
-			.remove_card_at(card_position)
-			.and_then(|card| card.play(table, &mut self.player))
-		{
-			println!("{}", n);
-			self.num_cards_played += 1;
-		}
-	}
-
-	fn terminate(mut self, table: &mut VecDeque<Player>, discard_pile: &mut Deck) {
-		// A player is not allowed to have more than 7 cards in their hand at the end of a turn.
-		// This needs to be checked at the end of each turn. The player should be propmted for discarding.
-		let mut to_be_discarded: i8 = self.player.hand.len() as i8 - 7;
-
-		println!("{} card(s) need to be discarded.", to_be_discarded);
-
-		while to_be_discarded > 0 {
-			self.player.hand.print_numbered();
-
-			if let Some(card) = input("> ")
-				.trim()
-				.parse::<u8>()
-				.ok()
-				.and_then(|i| self.player.remove_card_at(i))
-			{
-				discard_pile.push_back(card);
-                to_be_discarded -= 1;
-			}
-		}
-
-		table.push_back(self.player);
-	}
 }
 
 #[derive(Debug)]
--- src/game/mod.rs
+++ src/game/mod.rs
@@ -1,3 +1,6 @@
 mod game;
+mod turn;
 
 pub use game::{read_color, Game};
+use game::PlayerAction;
+use turn::Turn;
